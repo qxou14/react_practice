715bd145ab4b4193709e7f44a48746352454fd5c
QianXing Ou <qxou14@gmail.com>
2021-11-02 21:40:49 -0400

    css changes

--- pages/User.js
+++ pages/User.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Link from "next/link";
+import Script from "next/script";
+
 //getStaticProps
 class User extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      List: []
+      List: [],
+      show_info: 0,
     };
   }
 
@@ -20,17 +23,36 @@ class User extends React.Component {
       });
   }
 
+  // mouseEnter = (id) => {
+  //   this.setState({
+  //     show_info: 1,
+  //   });
+  // };
+
+  // mouseLeave = () => {
+  //   this.setState({
+  //     show_info: 0,
+  //   });
+  // };
+
   render() {
     var { List } = this.state;
+    var { show_info } = this.state;
 
     return (
       <div>
+        <h1>{show_info}</h1>
         {List.map((user) => (
           <p className="pointer">
-          <Link href={"/Details/" + user.id} key={user.id}>
-            <b className="color">{user.name}</b>
-          </Link>
-          <hr></hr>
+            <Link href={"/Details/" + user.id} key={user.id}>
+              <b
+                className="color"
+              >
+                {user.name}
+              </b>
+            </Link>
+            
+            <hr></hr>
           </p>
         ))}
       </div>
